146154f529fd31bc6b3993c7d9b6eb5fc1bf59b8
chryswoods <chryswoods@gmail.com>
2019-12-20 15:32:48 +0000

    Added a nicer loading animation for the application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React, { Suspense, lazy } from 'react';
-
 import ReactDOM from "react-dom";
+import Spinner from 'react-spinkit';
 
 import 'vis-network/dist/vis-network.min.css';
 import './index.css';
@@ -11,7 +11,18 @@ const SocialApp = lazy(()=>import('./SocialApp'));
 function App(props) {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={
+          <div className="brunel-center-container">
+            <div>Loading application...</div>
+            <div>
+              <Spinner
+                name="ball-grid-pulse"
+                color="green"
+                fadeIn="none"
+              />
+            </div>
+          </div>
+        }>
         <SocialApp/>
       </Suspense>
     </div>
