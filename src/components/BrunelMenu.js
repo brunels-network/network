f405e2a7cb2d9c4dc60554d93b4c2602db3b9853
Gareth Jones <oxidising@gmail.com>
2020-04-28 11:29:13 +0100

    WIP: Moving functions to BrunelMenu component

--- src/components/BrunelMenu.js
+++ src/components/BrunelMenu.js
@@ -1,44 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import styles from "./BrunelMenu.module.css";
 
-function BrunelMenu(props) {
-  if (!props.items) {
-    return <div>No menu</div>;
+class BrunelMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.viewAbout = this.viewAbout.bind(this);
+    this.viewSource = this.viewSource.bind(this);
   }
 
-  let buttons = [];
-
-  props.items.forEach((item, index) => {
-    let func = item[1];
-
-    if (props.emitClose) {
-      func = () => {
-        props.emitClose();
-        item[1]();
-      };
-    }
-
-    buttons.push(
-      <button
-        key={index}
-        className={styles.menuItem}
-        onClick={() => {
-          func();
-        }}
-      >
-        {item[0]}
-      </button>
+  viewAbout() {
+    let item = (
+      <div>
+        <div>
+          This is a demo of the application to view Brunel&apos;s temporal social network. This is an incomplete
+          application containing incomplete data and should not be relied on or viewed as being accurate.
+        </div>
+        <div>
+          If you want more information, then please visit the{" "}
+          <a href="https://github.com/chryswoods/brunel">GitHub repository</a>
+        </div>
+      </div>
     );
-  });
 
-  return <div className={styles.menu}>{buttons}</div>;
+    this.props.setOverlay(item);
+  }
+
+  viewSource() {
+    var win = window.open("https://github.com/gareth-j/brunel", "_blank");
+    win.focus();
+  }
+
+  render() {
+    return (
+      <div className={styles.menu}>
+        <button key="about" className={styles.menuItem} onClick={this.viewAbout}>
+          About
+        </button>
+        <button key="about" className={styles.menuItem} onClick={this.viewSource}>
+          Source
+        </button>
+        <button key="about" className={styles.menuItem} onClick={this.props.clickReset}>
+          Reset
+        </button>
+      </div>
+    );
+  }
 }
 
 BrunelMenu.propTypes = {
-  items: PropTypes.array,
-  emitClose: PropTypes.func,
+  clickReset: PropTypes.func.isRequired,
+  setOverlay: PropTypes.func.isRequired,
 };
 
 export default BrunelMenu;
