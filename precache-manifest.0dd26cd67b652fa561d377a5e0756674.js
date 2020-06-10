a53eac58cf99d8ba2a272705ee7068902770412a
Gareth Jones <oxidising@gmail.com>
2020-06-10 09:49:14 +0100

    Updates

---
+++ precache-manifest.0dd26cd67b652fa561d377a5e0756674.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b07cd55449d374869580d602a67c48",
+    "url": "/network/index.html"
+  },
+  {
+    "revision": "6e8524d8b4a52c24408d",
+    "url": "/network/static/css/2.9e81da96.chunk.css"
+  },
+  {
+    "revision": "11c6d85285577434ee87",
+    "url": "/network/static/css/3.38f073bb.chunk.css"
+  },
+  {
+    "revision": "7def0a75809a652fe300",
+    "url": "/network/static/css/4.80ca4c0b.chunk.css"
+  },
+  {
+    "revision": "3b65953535dd0d540082",
+    "url": "/network/static/css/main.e848410b.chunk.css"
+  },
+  {
+    "revision": "6e8524d8b4a52c24408d",
+    "url": "/network/static/js/2.9e8a3039.chunk.js"
+  },
+  {
+    "revision": "b0174c1b486777f7c4ed80d23f75d8f2",
+    "url": "/network/static/js/2.9e8a3039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c6d85285577434ee87",
+    "url": "/network/static/js/3.f19b4148.chunk.js"
+  },
+  {
+    "revision": "7def0a75809a652fe300",
+    "url": "/network/static/js/4.b68a9259.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/network/static/js/4.b68a9259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b65953535dd0d540082",
+    "url": "/network/static/js/main.f8e8f116.chunk.js"
+  },
+  {
+    "revision": "e1938381c079d94bba3e",
+    "url": "/network/static/js/runtime-main.b7a1914c.js"
+  },
+  {
+    "revision": "898c32f62d6dc1f8b9267cc34a635ce3",
+    "url": "/network/static/media/320px-SS_Great_Britain_diagram.898c32f6.jpg"
+  },
+  {
+    "revision": "7767cb60930c3760d254e2e1a00d72e3",
+    "url": "/network/static/media/320px-SS_Great_Britain_transverse_section.7767cb60.jpg"
+  },
+  {
+    "revision": "89cc4cbe1e76f9a7f957536b501d62b8",
+    "url": "/network/static/media/800px-Gutenberg_Bible.89cc4cbe.jpg"
+  },
+  {
+    "revision": "6240f5ced0bea5067aa679542539a010",
+    "url": "/network/static/media/A_Specimen_by_William_Caslon.6240f5ce.jpg"
+  },
+  {
+    "revision": "ee7f061c1e5e157c6a607b53e596458a",
+    "url": "/network/static/media/Brunel_letter_GWR.ee7f061c.jpg"
+  },
+  {
+    "revision": "234ffaa5742f7fe06585709d4780d935",
+    "url": "/network/static/media/Cinzel-Bold.234ffaa5.ttf"
+  },
+  {
+    "revision": "f85d382ea36969ec48effa613cd8a54d",
+    "url": "/network/static/media/Great_Western_maiden_voyage.f85d382e.jpg"
+  },
+  {
+    "revision": "946104e0f266e57a765a51eead7e4548",
+    "url": "/network/static/media/IllustriteZeitung_SS_GreatBritain.946104e0.png"
+  },
+  {
+    "revision": "73a0da7cb49ddf09cd1b2cc3ea480059",
+    "url": "/network/static/media/Joshua_field.73a0da7c.jpg"
+  },
+  {
+    "revision": "2cc98ebee00fb87618f4e894a3614d77",
+    "url": "/network/static/media/PlayfairDisplay-Bold.2cc98ebe.ttf"
+  },
+  {
+    "revision": "6b6f0053a6811a9381a9fa488ac76cb0",
+    "url": "/network/static/media/PlayfairDisplay-Medium.6b6f0053.ttf"
+  },
+  {
+    "revision": "b3721ba3bde34e5b38b0e1523cccfd7f",
+    "url": "/network/static/media/PlayfairDisplay-Regular.b3721ba3.ttf"
+  },
+  {
+    "revision": "6165164d7463f005bbf9db13fc92bac1",
+    "url": "/network/static/media/PlayfairDisplay-SemiBold.6165164d.ttf"
+  },
+  {
+    "revision": "c43448c2f851a4af0d0e93dd714bf95b",
+    "url": "/network/static/media/PlayfairDisplaySC-Bold.c43448c2.ttf"
+  },
+  {
+    "revision": "c3ec04838a1e59cd69bbeb803a6dfa61",
+    "url": "/network/static/media/PlayfairDisplaySC-Regular.c3ec0483.ttf"
+  },
+  {
+    "revision": "68e88d10b7f57e3ab523ee80b778cd1b",
+    "url": "/network/static/media/RHowlett_IKB.68e88d10.jpg"
+  },
+  {
+    "revision": "757d98b83f6ab4af56330b70c92a0019",
+    "url": "/network/static/media/Sumerian_26th_c_Adab.757d98b8.jpg"
+  },
+  {
+    "revision": "8756970fccd7e08d7beea18ebef2ad2a",
+    "url": "/network/static/media/The_Steamer_Great_Western_small.8756970f.jpg"
+  },
+  {
+    "revision": "a1a2d01bcd034270a3bc92176edfb66c",
+    "url": "/network/static/media/addNodeIcon.a1a2d01b.png"
+  },
+  {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "/network/static/media/backIcon.dd0baa69.png"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "/network/static/media/connectIcon.d5267b8d.png"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "/network/static/media/cross.260c9c65.png"
+  },
+  {
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "/network/static/media/deleteIcon.02d321ed.png"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "/network/static/media/editIcon.d5422321.png"
+  },
+  {
+    "revision": "ee48e16dfc01a21f57c5ef24cad1be3e",
+    "url": "/network/static/media/ssgb_logo.ee48e16d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/brunel/precache-manifest.5ccce55bf4943d2860dc7caa1fcd365b.js"
+  "/network/precache-manifest.0dd26cd67b652fa561d377a5e0756674.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brunel/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/network/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
