a12ad4992f36e5fb89427eadeb21601dc033278d
Christopher Woods <chryswoods@hey.com>
2020-11-12 19:50:55 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/network/precache-manifest.32fac939d1297ead7405261b3b5f40d2.js"
+  "/network/precache-manifest.842967d988c8d1d2dc3ef73b7988da2e.js"
 );
 
 self.addEventListener('message', (event) => {
